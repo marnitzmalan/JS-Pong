bec8a6bc75b396c70a0b3b23fffc73b7b932c9c8
Marnitz Malan <marnitz.malan@payfast.co.za>
2021-07-30 12:25:23 +0200

    render paddles

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+/**
+ * All Variables
+ */
+
 // Canvas
 const { body } = document;
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 
-const width = 500;
-const height = 700;
+// Canvas Size
+const canvasWidth = 500;
+const canvasHeight = 700;
 
-// Render Everything on Canvas
+// Paddle Size
+const paddleHeight = 10;
+const paddleWidth = 50;
+
+// Paddle position
+let paddleBottomX = 225;
+let paddleTopX = 225;
+
+
+/**
+ * Render Everything on Canvas 
+ */
 function renderCanvas() {
-  // TODO: render canvas functionality 
+  // Canvas Background
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, canvasWidth, canvasHeight); // fillRect(x, y, width, height)
+
+  // Paddle Color
+  context.fillStyle = 'white';
+
+  // Paddle Top (Computer)
+  context.fillRect(paddleTopX, 10, paddleWidth, paddleHeight);
+
+  // Paddle Bottom (Player)
+  context.fillRect(paddleBottomX, canvasHeight - 20, paddleWidth, paddleHeight);
 }
 
-// Create Canvas Element
+
+/**
+ * Create Canvas Elements
+ */
 function createCanvas() {
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = canvasWidth;
+  canvas.height = canvasHeight;
   body.appendChild(canvas);
   renderCanvas();
 }
